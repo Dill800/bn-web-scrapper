fc5e75c0b73adb46a9251a26351133a0ad39b6ee
Dillon McGovern <dill800@LAPTOP-O1V5GGUE.localdomain>
2018-09-15 20:55:56 -0400

    Second commit, added custom number of entries

--- BookCrawler.py
+++ BookCrawler.py
@@ -4,9 +4,12 @@ import pause
 
 bookAmount = int(input("Enter amount of books you wish to see: "))
 
-for page in range(1, int(bookAmount / 21) + 2):
+pageCap = bookAmount // 20
+pageRem = bookAmount % 20
 
-    response = get('https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8?Nrpp=20&page=' + str(page))
+for page in range(0, int(pageCap) + 1):
+
+    response = get('https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8?Nrpp=20&page=' + str(page + 1))
 
     # Start Parsing
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -26,14 +29,20 @@ for page in range(1, int(bookAmount / 21) + 2):
         dates.append(book.div.h3.span.text)
         prices.append(book.find('div', class_='product-shelf-pricing mt-s mb-s').tbody.tr.span.a.text)
 
-    print("B&N Top 100 Book Bestsellers:")
-
-    for index, book in enumerate(book_container):
-        print("Rank: " + ranks[index])
-        print("Title: " + titles[index])
-        print("\t" + authors[index])
-        print("Published: " + dates[index])
-        print("Price: " + prices[index] + "\n")
+    if page < pageCap:
+        for index, book in enumerate(book_container):
+            print("Rank: " + ranks[index])
+            print("Title: " + titles[index])
+            print("\t" + authors[index])
+            print("Published: " + dates[index])
+            print("Price: " + prices[index] + "\n")
+    else:
+        for index in range(0, pageRem):
+            print("Rank: " + ranks[index])
+            print("Title: " + titles[index])
+            print("\t" + authors[index])
+            print("Published: " + dates[index])
+            print("Price: " + prices[index] + "\n")
 
     # Pause retrieving info from website so servers don't get strained
-    pause.sleep(5)
+    pause.sleep(2)
